837a19787d388a669021b5cf41e515893a82968f
Chuck Jungmann <chuckj60@gmail.com>
2020-01-28 08:35:50 -0600

    Changed header value to a pointer to allow for unvalued headers.  Beginning of new strategy of making generic line reader to allow stdin and also, perhaps, memory-resident lines of recipients and headers.

--- mailcb.h
+++ mailcb.h
@@ -18,9 +18,21 @@ typedef struct _field_value
 typedef struct _header_field
 {
    const char           *name;
-   FieldValue           value;
+   FieldValue           *value;
    struct _header_field *next;
 } HeaderField;
+
+struct _line_reader;
+
+typedef int (*lr_next_line)(struct _line_reader *lr, const char **line, int *line_len);
+typedef int (*lr_current_line)(struct _line_reader *lr, const char **line, int *line_len);
+
+typedef struct _line_reader
+{
+   void            *data;
+   lr_next_line    next_line;
+   lr_current_line current_line;
+} LineReader;
 
 
 /**
